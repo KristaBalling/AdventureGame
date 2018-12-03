b27c79dc556a2df6d53cb6b2b7ed857d7b70800c
Krista Blachian <krista.blachian@scifusion360.com>
2018-12-03 10:31:05 -0500

    added flexibility to cmd line input using .split()

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -32,6 +32,13 @@ public class Main {
     locations.get(5).addExit("S", 1);
     locations.get(5).addExit("W", 2);
 
+    Map<String, String> vocabulary = new HashMap<String, String>();
+    vocabulary.put("QUIT", "Q");
+    vocabulary.put("NORTH", "N");
+    vocabulary.put("SOUTH", "S");
+    vocabulary.put("WEST", "W");
+    vocabulary.put("EAST", "E");
+
 
     int loc = 1;
     while(true) {
@@ -49,17 +56,24 @@ public class Main {
 
 
         String direction = scanner.nextLine().toUpperCase();
+        if(direction.length() > 1) {
+            String[] words = direction.split(" ");
+            for(String word: words) {
+                if(vocabulary.containsKey(word)) {
+                    direction = vocabulary.get(word);
+                    break;
+                }
+            }
+        }
 
-        if (exits.containsKey(direction)) {
+        if(exits.containsKey(direction)) {
             loc = exits.get(direction);
         } else {
             System.out.println("You cannot go in that direction");
         }
     }
-        loc = scanner.nextInt();
-        if(!locations.containsKey(loc)) {
-            System.out.println("You cannot go in that direction");
-        }
+
+
     }
 
         }
